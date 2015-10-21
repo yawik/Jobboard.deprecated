ebcccaccfcf2308c32dd6a26ec05960d1c9c8720
Carsten Bleek <bleek@cross-solution.de>
2015-10-21 21:40:10 +0200

    aktiviert regional search form

--- config/module.config.php
+++ config/module.config.php
@@ -6,55 +6,70 @@
 
 \YawikDemoJobboard\Module::$isLoaded = true;
 
-return array('view_manager' => array(
-    'template_map' => array(
-        'layout/layout'        => __DIR__ . '/../view/layout.phtml',
-        'core/index/index'     => __DIR__ . '/../view/index.phtml',
-        'piwik'                => __DIR__ . '/../view/piwik.phtml',
-        'content/about'       => __DIR__ . '/../view/about.phtml',
-        'main-navigation'      => __DIR__ . '/../view/main-navigation.phtml',
-        'jobs/form/list-filter' => __DIR__ . '/../view/search-for-jobs.phtml',
-        'templates/default/index' => __DIR__ . '/../view/templates/default/index.phtml'
+return array(
+    'service_manager' => array(
+        'factories' => array(
+            'YawikDemoJobboard/Listener/DelayedUserRegistrationMailSender'
+            => 'YawikDemoJobboard\Factory\Listener\DelayedUserRegistrationMailSenderFactory',
+        ),
     ),
-),
-             'translator'   => array(
-                 'translation_file_patterns' => array(
-                     array(
-                         'type'     => 'gettext',
-                         'base_dir' => __DIR__ . '/../language',
-                         'pattern'  => '%s.mo',
-                     ),
-                 ),
-             ),
-             'router'       => array(
-                 'routes' => array(
-                     'lang' => array(
-                         'options' => array(
-                             'defaults' => array(
-                                 'controller' => 'Jobs/Jobboard', //Overwrites the route of the start Page
-                                 'action'     => 'index',
-                             ),
-                         ),
-                     ),
-                 ),
-             ),
-             'acl' => array(
-                 'rules' => array(
-                     // guests are allowed to see a list of companies.
-                     'guest' => array(
-                         'allow' => array(
-                             'route/lang/organizations',
-                         ),
-                     ),
-                 ),
-             ),
-             'navigation' => array(
-                 'default' => array(
-                     'about'    => array(
-                         'label' => 'About',
-                         'order' => 200,
-                         'uri'   => '/de/content/about'
-                     ),
-                 ),
-             ),
+
+    'controllers' => array(
+        'invokables' => array(
+            'Jobs/Manage' => 'YawikDemoJobboard\Controller\JobsManageController',
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_map' => array(
+            'layout/layout'        => __DIR__ . '/../view/layout.phtml',
+            'piwik'                => __DIR__ . '/../view/piwik.phtml',
+            'content/about'       => __DIR__ . '/../view/about.phtml',
+            'content/applications-privacy-policy' => __DIR__ . '/../view/disclaimer.phtml',
+            'main-navigation'      => __DIR__ . '/../view/main-navigation.phtml',
+            'jobs/form/list-filter' => __DIR__ . '/../view/search-for-jobs.phtml',
+            'templates/default/index' => __DIR__ . '/../view/templates/default/index.phtml',
+            'content/jobs-terms-and-conditions' => __DIR__ . '/../view/agb.phtml',
+        ),
+    ),
+    'translator'   => array(
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    'router'       => array(
+        'routes' => array(
+            'lang' => array(
+                'options' => array(
+                    'defaults' => array(
+                        'controller' => 'Jobs/Jobboard', //Overwrites the route of the start Page
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'acl' => array(
+        'rules' => array(
+            // guests are allowed to see a list of companies.
+            'guest' => array(
+                'allow' => array(
+                    'route/lang/organizations',
+                ),
+            ),
+        ),
+    ),
+    'navigation' => array(
+        'default' => array(
+            'about'    => array(
+                'label' => 'About',
+                'order' => 200,
+                'uri'   => '/de/content/about'
+            ),
+        ),
+    ),
 );
